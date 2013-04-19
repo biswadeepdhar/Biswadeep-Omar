c0a924af53266854a86d394fb59f9b40921774ef
Omar Saucedo <saucedo.omar@yahoo.com>
2013-04-19 12:34:50 -0400

    Homework Update

--- homework.R
+++ homework.R
@@ -1,2 +1,11 @@
-print("hi")
-print("hello")
+getwd()
+setwd("~/DrPulliam/")
+
+calendar<-read.csv("calendar.csv")
+
+head<-read.table("testHeads.o")
+
+bin<-read.table("testbin.o")
+
+plot(calendar) 
+
